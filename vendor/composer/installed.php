16152e10c75c67cb79dc3d2e4c4ca505721d6224
Dika Neutron <dika.neutron@gmail.com>
2024-11-12 10:19:37 +0700

    Release Version Release Code of v1.24.45.1 Beta

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f77d4007f31b46435410a7dc834267c8d1165248',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f77d4007f31b46435410a7dc834267c8d1165248',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/authentication-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'codeigniter4/settings' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '2748f2b4572d44a940f98c31847d65272cac5666',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeigniter4/settings',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/shield' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '22a8b3b58dafa7a5c080bc61446653aeb9fffc06',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeigniter4/shield',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
